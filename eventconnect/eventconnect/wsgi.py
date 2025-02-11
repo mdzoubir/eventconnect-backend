ac1f8dfd02a06b1fd3acaef0abb1045432882c0b
Mohamed zoubir <simozoubir012@gmail.com>
2025-02-11 20:56:40 +0100

    sys.path.append

--- eventconnect/eventconnect/wsgi.py
+++ eventconnect/eventconnect/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 if os.environ.get('VERCEL') == '1' or os.environ.get('VERCEL_ENV') == 'production':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventconnect.eventconnect.settings')
