f53dac7c5464305be93b5b2561e6ce2b1fea7216
Mohamed zoubir <simozoubir012@gmail.com>
2025-02-11 21:20:37 +0100

    change the structure

---
+++ vercel_app.py
@@ -0,0 +1,9 @@
+# vercel_app.py
+import os
+import sys
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
+from eventconnect.wsgi import application
