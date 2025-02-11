fe361ef3b4784eb4e2e8eb244862db399bdc0a6b
Mohamed zoubir <simozoubir012@gmail.com>
2025-02-11 20:42:16 +0100

    change debug to false

--- eventconnect/eventconnect/settings.py
+++ eventconnect/eventconnect/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)m&3&hu0=+s0!x65lilhuwo!s33npu%yw%muk26xo#crng1bes')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'eventconnect-backend.vercel.app']
 
 # Application definition
 
